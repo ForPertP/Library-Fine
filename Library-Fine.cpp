9c184d77e5b937ba4c958642d439e738678d4272
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2023-02-13 14:24:53 +0900

    Update Library-Fine.cpp

--- Library-Fine.cpp
+++ Library-Fine.cpp
@@ -1,3 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'libraryFine' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER d1
+ *  2. INTEGER m1
+ *  3. INTEGER y1
+ *  4. INTEGER d2
+ *  5. INTEGER m2
+ *  6. INTEGER y2
+ */
+
 int libraryFine(int d1, int m1, int y1, int d2, int m2, int y2)
 {
     if (y1 > y2) return 10000;
